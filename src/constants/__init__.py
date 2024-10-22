1d0c39ad43fe2d0601ca19d7d3ebbca61e94fb74
surajojha668 <surajojha2626@gmail.com>
2024-10-22 16:53:42 +0530

    New Update

---
+++ src/constants/__init__.py
@@ -0,0 +1,7 @@
+# constans folder is used to used to store any constants used accross the file
+
+MONGODB_URL_KEY: str = "mongodb+srv://surajojha2626:12345@cluster0.mubxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
